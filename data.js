16ac03f98ccce6e8d5c335071eedc5957b5f2430
aranscope <aranlong@live.co.uk>
2016-09-21 12:03:21 -0400

    added topics

--- data.js
+++ data.js
@@ -4,32 +4,53 @@ var fs = require('fs')
 var bodyParser = require('body-parser');
 var cors = require('cors')
 app.use(bodyParser.json());
-app.use(cors({origin: 'null'}));
+app.use(cors({origin: '*'}));
 
 function question_bank() {
         this.topic = ''
         this.questions = []
 }
 
+Array.prototype.contains = function(item) {
+	return this.indexOf(item) > -1
+}	
+
 function load_question_bank(path) {
         var question_bank_file = fs.readFileSync(path)
-        return JSON.parse(question_bank_file)
+        var qbank = JSON.parse(question_bank_file)
+	console.log(qbank)
+	return qbank
 }
-
 
 function save_question(question) {
         var path = './topics/' + question.topic + '.txt'
 
         var qbank = new question_bank()
 
-        if(fs.existsSync(path)) qbank = load_question_bank(path)
+        if(fs.existsSync(path)){
+        	qbank = load_question_bank(path)
+        } 
+	qbank.topic = question.topic
+	if(question.questions.length > 0) {
+		var index = -1
+		for(i in qbank.questions) {
+			if(qbank.questions[i].id == question.questions[0].id) {
+				index = i
+				break
+			}
+		}
 
-        qbank.questions.push(question.question)
-
+		if(index > -1) {
+			qbank.questions[index].children.push(question.questions[0].children[0])
+		}
+		else {
+			qbank.questions.push(question.questions[0])
+		}	
+	}
         fs.writeFile(path, JSON.stringify(qbank), function(err) {
             if (err) console.log(err)
-            console.log('question saved.');
-    });
+            console.log('question saved.')
+   	 });
 }
 
 app.post('/questions', function(req, res) {
@@ -45,22 +66,32 @@ app.get('/', function(req, res) {
 })
 app.get('/topics', function(req, res) {
         console.log('get request')
-        var files = fs.readdirSync('./topics')
+        var files = fs.readdirSync('./topics/')
 	var file_names = []
-	for(i in files) {
-		file_names.push(files[i].replace('.txt', ''))
-	}	
+	console.log(files)
+	
+	if(files.length > 0) {
+		for(var i = 0; i < files.length;i++) {
+			var file_name = files[i];
+			console.log(file_name)
+			file_names.push(file_name.replace('.txt', ''))
+		}
+	}
+	
 
         res.send(file_names)
 })
 
 app.get('/questions/:topic', function(req, res) {
+	console.log('request questions for topic: ' + req.params.topic)
 	var path = './topics/' + req.params.topic + '.txt'
 	if(fs.existsSync(path)) {
+		console.log('loading question bank')
 		res.send(load_question_bank(path))
 	} else {
+		console.log('topic does not exist')
 		res.send('topic does not exist')
 	}
 })
 
-app.listen(3001)
+app.listen(80)
